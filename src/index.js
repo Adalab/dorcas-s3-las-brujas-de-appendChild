4260f7b1fda29209c6cadb301b0356044d9d0e36
Maria Violeta <mariavioleta1417@gmail.com>
2018-07-24 19:52:13 +0200

    fontAwsomw

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// import fontAwesome from ‘@fortawesome/fontawesome-free/css/all.css’;
+import fontAwesome from '@fortawesome/fontawesome-free/css/all.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
