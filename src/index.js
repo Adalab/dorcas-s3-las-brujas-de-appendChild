d9e6a349cf781fda3d7a54c0667cf1f31890839c
Maria Violeta <mariavioleta1417@gmail.com>
2018-07-25 19:54:00 +0200

    add css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './main.css';
 import registerServiceWorker from './registerServiceWorker';
 import fontAwesome from '@fortawesome/fontawesome-free/css/all.css';
 
