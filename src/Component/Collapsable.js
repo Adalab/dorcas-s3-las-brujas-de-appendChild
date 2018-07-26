35234841f36d2a10d36e4fa88e651282d8d54133
oljuanfe <ojuanferrero@gmail.com>
2018-07-26 20:16:09 +0200

    Change html and classes of collapsables headers in order to match

--- src/Component/Collapsable.js
+++ src/Component/Collapsable.js
@@ -3,13 +3,15 @@ import Form from './Form'
 
 class Collapsable extends Component {
   render() {
-    return (    <fieldset className="design__container js-collapsible-visible js-collapsible-select">
-          <div className="design__title js-collapsible-title">
-            <legend className="legend__title design-title__legend">
-              <i className="icon-title far fa-object-ungroup"></i> diseña
-            </legend>
-            <span>
-              <i className="design__icon-legend fas fa-chevron-down jsRotate"></i>
+    return (
+         <fieldset className="fieldset__container js-collapsible-visible js-collapsible-select">
+          <div className="section-collapsible--title js-collapsible-title">
+            <span className="icon-title">
+              <i className="far fa-object-ungroup"></i>
+            </span>
+            <legend className="legend__title">diseña</legend>
+            <span className="icon-down">
+              <i className="fas fa-chevron-down jsRotate"></i>
             </span>
           </div>
           {this.props.children}
