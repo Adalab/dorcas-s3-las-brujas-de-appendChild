d33a4ec6bb8ceb18bc95466e5b2d63809eab8433
bismilahlf <bismilah.lf@gmail.com>
2018-07-26 20:25:17 +0200

    Create structure

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Preview from './Component/preview';
 import Form from './Component/Form';
 
+
 class Main extends Component {
   render() {
     return (
