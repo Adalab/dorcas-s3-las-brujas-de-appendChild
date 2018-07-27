0f8a10b515e24c88a7c0db71cd3b3ca4fd16da9f
Maria Violeta <mariavioleta1417@gmail.com>
2018-07-27 19:28:15 +0200

    add object

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import Preview from './Component/preview';
 import Form from './Component/Form';
-
+const objetoTarjeta = {
+  email:"",
+  github:"",
+  job:"Puesto de hola Trabajo",
+  linkedin:"",
+  name:"Nombre Completo",
+  palette:"1",
+  phone:"",
+  photo:"images/image-card.png",
+  typography:2,
+  skills:[] 
+}
 
 class Main extends Component {
   render() {
     return (
       <div>
       <Preview
-        reset="RESET"
-        firstName="Nombre Apellido"
-        job="Profesión"
+        skills={objetoTarjeta.skills}
+        job={objetoTarjeta.job}
+        firstName={objetoTarjeta.name}
+        mail={objetoTarjeta.email}
       />
       <Form />
       </div>
