aab7af63063513811758efd4e00dd2a06bd1a89b
Bismilah <bismilah@Juans-MacBook-Pro-2.local>
2018-08-05 18:41:25 +0200

    Add React router in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import CardGenerator from './Component/CardGenerator';
 import LandingPage from './Component/LandingPage';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <LandingPage/>
-        <CardGenerator/>
-      </div>
+        <Switch>
+            <Route exact path='/' component= { LandingPage } />
+            <Route path='/card-generator' component= { CardGenerator } />
+        </Switch>
     )
   }
 }
