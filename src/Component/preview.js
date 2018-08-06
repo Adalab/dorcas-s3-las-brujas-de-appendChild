85453a0a9f559c2567d12a7136c4c3c78d8f5fc1
Laura <laurav.zarco@gmail.com>
2018-08-06 20:01:34 +0200

    Nombre completo in preview

--- src/Component/preview.js
+++ src/Component/preview.js
@@ -3,20 +3,20 @@ import Icon from './Icon';
 import PropTypes from 'prop-types';
 
 class Preview extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    console.log('props',props);
+    console.log('props', props);
   }
-  resetButtom(){
-  console.log("me han pulsado");
+  resetButtom() {
+    console.log("me han pulsado");
   }
-  render(){
+  render() {
     return (
       <section className="section-card">
         <button onClick={this.resetButtom}
-          type="button" 
-          name="Reset" 
-          alt="reset button" 
+          type="button"
+          name="Reset"
+          alt="reset button"
           className="resetbutton"
         >
           <i className="far fa-trash-alt">
@@ -27,48 +27,48 @@ class Preview extends Component {
         </button>
         <div className={`card jsCard ${this.props.colorPalette}`} >
           <div className="card__top">
-           <div className="card-info js-card-info">
-              <p 
-              className="card__top--name js-card__top--name" 
-              id="fullname">{this.props.firstName}
+            <div className="card-info js-card-info">
+              <p
+                className="card__top--name js-card__top--name"
+                id="fullname">{this.props.firstName || 'Nombre Completo'}
               </p>
-              <p 
-              className="card__top--career js-card__top--career" 
-              id="profesion">
-              {this.props.job}
+              <p
+                className="card__top--career js-card__top--career"
+                id="profesion">
+                {this.props.job}
               </p>
             </div>
           </div>
-        <div className="card__image"></div>
-        <ul className="card__social">
-          <Icon 
-            classA="button--mobile" 
-            id="mobile-link"    
-            classI= "fas fa-mobile-alt"  
-            title="número de teléfono"  
-            link={`tlf: ${this.props.telf}`} 
-          />
-          <Icon 
-          classA="button--mail"  
-          id="mail-link"      
-          classI= "far fa-envelope"     
-          title="correo electrónico"  
-          link={`mailto: ${this.props.mail}`} 
-          />
-          <Icon 
-          classA="button--linkedin"  
-          id="linkedin-link"  
-          classI= "fab fa-linkedin-in"  
-          title="Linkedin"        
-          link={`https://www.linkedin.com/in/ ${this.props.linkedin}`} 
-          />
-          <Icon 
-          classA="button--github" 
-          id="github-link"    
-          classI= "fab fa-github-alt"   
-          title="Github"            
-          link={`https://github.com/ ${this.props.github}`} />
-        </ul> 
+          <div className="card__image"></div>
+          <ul className="card__social">
+            <Icon
+              classA="button--mobile"
+              id="mobile-link"
+              classI="fas fa-mobile-alt"
+              title="número de teléfono"
+              link={`tlf: ${this.props.telf}`}
+            />
+            <Icon
+              classA="button--mail"
+              id="mail-link"
+              classI="far fa-envelope"
+              title="correo electrónico"
+              link={`mailto: ${this.props.mail}`}
+            />
+            <Icon
+              classA="button--linkedin"
+              id="linkedin-link"
+              classI="fab fa-linkedin-in"
+              title="Linkedin"
+              link={`https://www.linkedin.com/in/ ${this.props.linkedin}`}
+            />
+            <Icon
+              classA="button--github"
+              id="github-link"
+              classI="fab fa-github-alt"
+              title="Github"
+              link={`https://github.com/ ${this.props.github}`} />
+          </ul>
         </div>
       </section>
     );
